67029353d9ab26d42c5aa51220d6825c0039c3e9
LeiderGalindo <000999zzz44@gmail.com>
2022-03-25 23:28:32 -0500

    commit inicial

---
+++ pages/pokemones/[id].js
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+
+const Pokemon = ({ data }) => {
+    const router = useRouter()
+    console.log(router);
+    //siempre y cuando en la parte de fallback este este en true
+    // if (router.isFallback){
+    //     return (
+    //         <p>Cargando...</p>
+    //     )
+    // }
+
+    return (
+        <div>
+            <h1>{data.name} número #{data.id}</h1>
+            <Image src={data.sprites.front_default} width={400} height={400} />
+            <Link href='/'>Volver al inicio</Link>
+        </div>
+    )
+}
+
+export default Pokemon
+
+export const getStaticProps = async ({ params }) => {
+    const response =  await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+    const data = await response.json()
+
+    return { props: { data } }
+}
+
+export const getStaticPaths = async () => {
+    const paths = [
+        { params: {id: '1'} },
+        { params: {id: '2'} },
+    ]
+
+    return {
+        paths,
+        fallback: 'blocking',//true
+    }
+}
+// export const getServerSideProps = async ({ params }) => {
+//     const response =  await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+//     const data = await response.json()
+
+//     return { props: { data } }
+// }
